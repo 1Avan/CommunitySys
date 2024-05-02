2955d4cf714c65b20e9188fc3e4031c3d05997fe
1Avan <131638111+1Avan@users.noreply.github.com>
2024-05-02 11:41:58 +0800

    Delete BackEndCode directory

--- BackEndCode/CommunityManagementSys/src/main/java/com/ivan/communitymanagementsys/constant/StatusConstant.java
+++
@@ -1,8 +0,0 @@
-package com.ivan.communitymanagementsys.constant;
-
-public class StatusConstant {
-    //用户是否正常（1.是 2.否）
-    public static final Integer ENABLE = 1;
-    public static final Integer DISABLE = 2;
-
-}
